81a5770396956fcc4e3c9c456fec1d4384f28c20
parameshwar <162430581+parameshwar21@users.noreply.github.com>
2025-12-25 07:25:55 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,14 +1,12 @@
 <?php
-// db.php - Database connection using Render environment variables
+$host = "127.0.0.1";
+$user = "root";
+$pass = "";
+$db   = "students_achieva";
+$port = 3306; 
 
-$host = getenv('DB_HOST');   // from Render environment variables
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');   // can be empty
-$db   = getenv('DB_NAME');
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
-$conn = new mysqli($host, $user, $pass, $db);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
